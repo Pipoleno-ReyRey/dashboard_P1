0ffe13c5dd39334887921d0ce4f8d1e8f151b065
Reynaldo A. Suero Delmonte <rasuero@plazalama.com>
2025-11-04 23:15:25 -0400

    product model

---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    sku!: string;
+    title!: string;
+    img!: string;
+    bigItem!: boolean;
+    matnr!: string;
+    infiniteStock!: boolean;
+    offShops!: string[];
+    status!: boolean;
+}
